ac8fb4d24db619271db1939422d2b91f93d9a307
liferabo <realmotinba6@gmail.com>
2016-12-08 00:02:51 +0900

    1ページ目のタイトル・はてぶ数・urlをスクレイピング

--- fuel/app/classes/controller/testform.php
+++ fuel/app/classes/controller/testform.php
@@ -1,9 +1,5 @@
 <?php
 
-require_once(dirname(__FILE__).'/../../../vendor/autoload.php');
-
-require_once(dirname(__FILE__).'/../../../vendor/twig/twig/lib/Twig/Autoloader.php');
-
 use Goutte\Client;
 
 class Controller_Testform extends Controller
@@ -18,10 +14,11 @@ class Controller_Testform extends Controller
     public function action_index()
     {
         // 入力値の出力先URLを生成・セット
-        $output_url = View::forge("testform/index");
-        $output_url->set("result_url", Uri::create("testform/getdata"));
+        $testform_object = View::forge("testform/index");
+//        var_dump($testform_object);
+        $testform_object->set("result_url", Uri::create("testform/getdata"));
 
-        return $output_url;
+        return $testform_object;
     }
 
     /**
@@ -32,23 +29,24 @@ class Controller_Testform extends Controller
     {
         $client = new Client();
 
-        $form_data = Input::get('keyword');
-
-        $crawling_url = $this::HATEB_SEARCH_URL . urlencode($form_data) . $this::HATEB_BOOKMARK_COUNT_50;
+        $crawling_url = $this::HATEB_SEARCH_URL . urlencode(Input::get('keyword')) . $this::HATEB_BOOKMARK_COUNT_50;
 
         $crawler = $client->request('GET', $crawling_url);
         sleep(1);
 
-        // タイトルをスクレイプ
-        $title = $crawler->filter("h3 a")->each(function($element){
-            echo $element->text()."\n";
-        });
-        // はてぶ数をスクレイプ
-        $hatdeb_count = $crawler->filter("span.users a")->each(function($element){
-            echo $element->text()."\n";
-        });
+        // はてぶ 1ページのURLを取得
+        $url = $crawler->filter("h3 a")->extract("href");
 
-        Twig_Autoloader::register();
+        $title = array();
+        $hateb_count = array();
+
+        // URL件数分の タイトル と はてぶ数 を取得
+        for($i=0; $i<count($url); $i++)
+        {
+            $title[] = $crawler->filter("h3 a")->eq($i)->text();
+            $hateb_count[] = $crawler->filter("span.users a")->eq($i)->text();
+        }
+
         $loader = new Twig_Loader_Filesystem(dirname(__FILE__).'/../../views/');
         $twig = new Twig_Environment($loader, array("cache" => "cache/", "debug" => true));
         $template = $twig->loadTemplate("base.html.twig");
--- fuel/app/views/testform/index.php
+++ fuel/app/views/testform/index.php
@@ -2,10 +2,7 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     </head>
-
-    <h1>クローリング</h1>
-
-    <p>入力フォーム</p>
+    <h1>入力フォーム</h1>
     <form method="get" action="<?php echo $result_url; ?>">
         <input type="text" name="keyword" id="keyword" value="">
         <input type="submit">
